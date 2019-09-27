b765560be21c4f02685a68b6262305f539938821
Michelle Pan <michelle-pan@users.noreply.github.com>
2019-09-26 20:25:20 -0700

    add 2019 code
    added basic 2019 CV code and modified test.py to accept image and video
    input types

---
+++ test.py
@@ -0,0 +1,33 @@
+import socket
+import time
+import os
+import subprocess
+import cv2
+import argparse
+from datetime import datetime
+from cv import VisionTargetDetector
+
+# "python test.py 0" to run from camera in port 0
+# "python test.py video.mp4" to run from the video recording video.mp4
+# "python test.py --out output.mp4" to save output to a recording file
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input", help="read from the given camera or video", default="0")
+args = parser.parse_args()
+
+print("Reading from", args.input)
+
+# df = VisionTargetDetector("0")
+# db = VisionTargetDetector("1")
+
+vtd = VisionTargetDetector(args.input)
+
+#with df, db as d1, d2:
+while True:
+    # angle, distance = d.run_cv()
+    # print(df.run_cv())
+    # print(db.run_cv())
+	print(vtd.run_cv())
+
+# close input and writer
+detector.release_cv_objects()
